971e8b6a14604af29823a5a572b2290665be1068
Aaron <aaronweiss8@gmail.com>
2020-04-20 23:56:48 -0400

    worked on chip

--- Chip.ml
+++ Chip.ml
@@ -1,12 +1,42 @@
 type color =
-  | White
-  | Red
-  | Blue
-  | Green
-  | Black
+  | White of int
+  | Red of int
+  | Blue of int
+  | Green of int
+  | Black of int
 
-type quantity = int
+type t = (color * color * color * color * color)
 
-type t = {color:color;
-          quantity:quantity}
+exception RI_Broken
+
+(**RI: type t must in the form White, Red, Blue, Green, Black *)
+let repo_ok = function
+  |((White w),(Red r),(Blue blu),(Green g),(Black bla)) as t -> t
+  | _ -> raise RI_Broken;;
+
+
+let get_value = function
+  |((White w),(Red r),(Blue blu),(Green g),(Black bla)) -> 
+    w + r*5 + blu*10 + g*25 + bla*100
+  |_ -> raise RI_Broken;;
+
+(**[single_step t] takes the lowest denominations available and converts them
+into the next denomination if possible.
+Returns: type (t, bool), where the bool indicates if type *)
+let single_step_simplify = function
+  |((White w),(Red r),(Blue blu),(Green g),(Black bla)) as t -> 
+    if w >= 5 
+    then (((White (w-5)),(Red (r+1)),(Blue blu),(Green g),(Black bla)),true)
+    else if r > 2
+    then (((White (w)),(Red (r-1)),(Blue (blu+1)),(Green g),(Black bla)),true)
+    else if r = 1 && blu >= 2
+    then (((White (w)),(Red (r-1)),(Blue (blu-2)),(Green (g+1)),(Black bla)),true)
+    else if g >= 4
+    then (((White (w)),(Red r),(Blue blu),(Green (g-2)),(Black (bla+1))),true)
+    else (t,false)
+  |_ -> raise RI_Broken
+
+  (* let single_step_break = function
+    |((White w),(Red r),(Blue blu),(Green g),(Black bla)) as t -> 
+    | _ -> raise RI_Broken *)
 
--- Chip.mli
+++ Chip.mli
@@ -3,13 +3,20 @@
 (*abstract type representing a chip*)
 type t
 
-(** options for color type*)
+(** options for color type, white is always worth 1, Red worth 5, Blue worth 10,
+Green worth 25, and Black worth 100, the int value is the quanity of chip*)
 type color =
-    | White
-    | Red
-    | Blue
-    | Green
-    | Black
+    | White of int
+    | Red of int
+    | Blue of int
+    | Green of int
+    | Black of int
 
-(** an int representing the quanity of a chip or chips, not value*)
-type quantity = int
+exception RI_Broken
+
+(**RI: type t must in the form White, Red, Blue, Green, Black. Returns type t
+if RI is okay and a failure mesage otherwise.*)
+val repo_ok: t -> t
+
+(**[get_value t] is the integer representation of the value of the chips *)
+val get_value: t -> int
